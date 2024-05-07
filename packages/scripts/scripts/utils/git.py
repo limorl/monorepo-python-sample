ff7ab1b1b7a9779c9e6e35717039768061ade19a
Limor Lahiani <limor@vox.studio>
2024-05-07 21:18:19 +0000

    upgrade to pytest 8.2.0

--- packages/scripts/scripts/utils/git.py
+++ packages/scripts/scripts/utils/git.py
@@ -2,6 +2,7 @@ import pathspec
 import os
 import subprocess
 
+
 def load_gitignore(root_dir):
     gitignore_path = os.path.join(root_dir, '.gitignore')
     patterns = []
@@ -9,6 +10,7 @@ def load_gitignore(root_dir):
         with open(gitignore_path, 'r') as file:
             patterns = file.readlines()
     return pathspec.PathSpec.from_lines('gitwildmatch', patterns)
+
 
 def get_changed_files(directory):
     changed_files = subprocess.check_output(
