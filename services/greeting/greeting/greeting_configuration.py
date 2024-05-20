2277f1c6bf7d06f211544306fdf718834138ff84
Limor Lahiani <limor.lahiani@gmail.com>
2024-05-20 11:50:04 +0300

    Update greeting_configuration.py

--- services/greeting/greeting/greeting_configuration.py
+++ services/greeting/greeting/greeting_configuration.py
@@ -3,3 +3,4 @@ from configuration.configuration import Configuration, ConfigurationSection
 
 class GreetingConfiguration(Configuration):
     def __init__(self, config_dict: ConfigurationSection):
+        self.num_of_exclamations: int = config_dict.get('num_of_exclamations')
