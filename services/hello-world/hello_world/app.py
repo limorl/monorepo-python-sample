903115bfc800a96c9f6aac9b5b92466a744d5cd7
Limor Lahiani <limor@vox.studio>
2024-06-03 21:32:08 +0000

    add RUFF lnting + fixes. Add AppConfiguration to GreetingService

--- services/hello-world/hello_world/app.py
+++ services/hello-world/hello_world/app.py
@@ -3,7 +3,4 @@ from typing import Any
 
 
 def lambda_handler(event, context) -> Any:  # noqa: ANN001, ARG001
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello World!')
-    }
+    return {'statusCode': 200, 'body': json.dumps('Hello World!')}
