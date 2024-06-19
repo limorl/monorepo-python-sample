4dd3796f8991f5324ed9abbf1e83890e7fd6ede0
Limor Lahiani <limor@vox.studio>
2024-06-19 09:23:57 +0000

    fix bug with terraform-docs

--- infra/terraform/modules/ecr-repository/main.tf
+++ infra/terraform/modules/ecr-repository/main.tf
@@ -1,19 +1,19 @@
 resource "aws_ecr_repository" "ecr_repository" {
-    name                 = var.ecr_repository_name
-    image_tag_mutability = var.image_mutability
+  name                 = var.ecr_repository_name
+  image_tag_mutability = var.image_mutability
 
-    encryption_configuration {
-        encryption_type = var.encrypt_type
-    }
+  encryption_configuration {
+    encryption_type = var.encrypt_type
+  }
 
-    image_scanning_configuration {
-        scan_on_push = true
-    }
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 
-    tags = merge(
-        var.tags,
-        {
-            Type = "ecr_repository"
-        },
-    )
+  tags = merge(
+    var.tags,
+    {
+      Type = "ecr_repository"
+    },
+  )
 }
--- infra/terraform/modules/ecr-repository/variables.tf
+++ infra/terraform/modules/ecr-repository/variables.tf
@@ -1,25 +1,25 @@
 variable "ecr_name" {
-    description = "The ECR registry name"
-    type        = string
-    default     = null
+  description = "The ECR registry name"
+  type        = string
+  default     = null
 }
 
 variable "ecr_repository_name" {
-    type        = string
-    default     = null
+  type    = string
+  default = null
 }
 
 variable "image_mutability" {
-    type        = string
-    default     = "MUTABLE"
+  type    = string
+  default = "MUTABLE"
 }
 
 variable "encrypt_type" {
-    type        = string
-    default     = "KMS"
+  type    = string
+  default = "KMS"
 }
 
 variable "tags" {
-    type        = map(string)
-    default     = {}
+  type    = map(string)
+  default = {}
 }
