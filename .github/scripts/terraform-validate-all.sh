7375240da3440a1abfb6d7c59e1857bcb9fd0422
Limor Lahiani <limor@vox.studio>
2024-06-29 07:37:53 +0000

    update terraform lock

---
+++ .github/scripts/terraform-validate-all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Cleaning up existing Terraform initialization..."
+find infra/terraform -type d -name ".terraform" -exec rm -rf {} +
+find infra/terraform -type f -name ".terraform.lock.hcl" -exec rm -f {} +
+
+echo "Initializing and validating main Terraform configuration..."
+terraform -chdir=infra/terraform init -backend=false -upgrade
+terraform -chdir=infra/terraform validate
+
+echo "Initialize and validate Terraform in each module directory"
+for dir in infra/terraform/modules/*; do
+  if [ -d "$dir" ]; then
+    echo "Initializing and validating Terraform module in $dir..."
+    terraform -chdir="$dir" init -backend=false -upgrade
+    terraform -chdir="$dir" validate
+  fi
+done
