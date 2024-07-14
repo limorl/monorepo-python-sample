61840cf028b4405e6e05b7632ce66b5e39900251
Limor Lahiani <limor@vox.studio>
2024-07-14 13:31:17 +0000

    update terraform service module to include ecr repository

--- infra/terraform/modules/service/main.tf
+++ infra/terraform/modules/service/main.tf
@@ -1,13 +1,13 @@
 module "appconfig_app" {
-  source       = "../appconfig_app"
-  app_name     = "${var.service_name}-service"
-  env_name    = var.stage
-  tags         = var.tags
+  source   = "../appconfig_app"
+  app_name = "${var.service_name}-service"
+  env_name = var.stage
+  tags     = var.tags
 }
 
 module "ecr_repository" {
-    source                  = "../ecr-repository"
-    ecr_repository_name     = var.service_name
-    tags                    = var.tags
+  source              = "../ecr-repository"
+  ecr_repository_name = var.service_name
+  tags                = var.tags
 }
 
