857e3d378521cd829fb23545a752218899dd1b39
Limor Lahiani <limor@vox.studio>
2024-07-17 17:14:14 +0000

    update terraform service modules

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -3,6 +3,7 @@
 
 module "services_primary" {
   source = "../../modules/services"
+  
   providers = {
     aws = aws.dev-primary
   }
--- infra/terraform/modules/lambda/main.tf
+++ infra/terraform/modules/lambda/main.tf
@@ -1,13 +1,13 @@
-resource "aws_lambda_function" "function" {
-  function_name = var.function_name
-  role          = aws_iam_role.lambda_role.arn
-  package_type  = "Image"
-  image_uri     = "${var.ecr_repository_url}:${var.image_tag}"
-}
-
 resource "aws_iam_role" "lambda_role" {
   name               = "${var.function_name}-lambda-role"
   assume_role_policy = file("${path.module}/assume-role-policy.json")
+  
+  tags = merge(
+    var.tags,
+    {
+      Type = "aws_iam_role"
+    },
+  )
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_basic" {
@@ -18,6 +18,19 @@ resource "aws_iam_role_policy_attachment" "lambda_basic" {
 resource "aws_iam_role_policy" "lambda_appconfig_secretsmanager_policy" {
   name = "${var.function_name}-lambda-appconfig-secretsmanager-policy"
   role = aws_iam_role.lambda_role.id
-
   policy = file("${path.module}/appconfig-secretesmanager-policy.json")
+}
+
+resource "aws_lambda_function" "function" {
+  function_name = var.function_name
+  role          = aws_iam_role.lambda_role.arn
+  package_type  = "Image"
+  image_uri     = "${var.ecr_repository_url}:${var.image_tag}"
+
+  tags = merge(
+    var.tags,
+    {
+      Type = "aws_lambda_function"
+    },
+  )
 }
