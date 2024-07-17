01416af9b9133071fe0628bc2062a3ccfe789fab
Limor Lahiani <limor@vox.studio>
2024-07-17 20:48:09 +0000

    add ecr permissions to lambda role

--- infra/terraform/modules/service/outputs.tf
+++ infra/terraform/modules/service/outputs.tf
@@ -2,3 +2,8 @@ output "lambda_role_arn" {
   description = "The ARN of the Lambda IAM role"
   value       = module.lambda_function.lambda_role_arn
 }
+
+output "ecr_repository_arn" {
+  description = "The ARN of the ECR repository"
+  value       = aws_ecr_repository.ecr_repository.arn
+}
